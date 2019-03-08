49a8f924ea36f10a6122698b6a33fe11dc18371b
Mahmoud Atwa <m.atwa@articlebox.net>
2019-03-08 14:54:24 +0200

    Initial boilerplate

---
+++ index.js
@@ -0,0 +1,131 @@
+/**
+ * @description This file should only call handler functions and specifying routes.
+ * @description It's also the entry/main file of our application which listens for everything.
+ * 
+ * @example
+ * const userService = require("./users");
+ * const Handler = new userService();
+ * app.get("/users", Handler.getUsers);
+ * 
+ * @note To use apiDoc, also don't commit the output file of apiDoc.
+ * @note It's ignored in the .gitignore file.
+ * @run npm install apidoc -g
+ * @start apidoc -i myapp/ -o apidoc/
+ * 
+ * @example above the handler fn.
+ * //////////////////////////////////////
+ * @api {get} /user/:id Request User information
+ * @apiName GetUser {method + endpoint/path}
+ * @apiGroup ServiceName
+ *
+ * @apiParam {Number} id Users unique ID. {if any}
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "firstname": "John",
+ *       "lastname": "Doe"
+ *     }
+ *
+ * @apiError UserNotFound The id of the User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "UserNotFound"
+ *     }
+ * //////////////////////////////////////
+ * @see http://apidocjs.com/
+ */
+
+ const app = require("express")();
+
+
+/**
+ * @name UserService
+ * @note These are the routes for anything related to a user.
+ * @note This is just general routing, You can modify as you want but before the delivery of the documentation
+ */
+app.get("/users", (req, res) => {});
+app.post("/users", (req, res) => {});
+app.put("/users", (req, res) => {});
+app.delete("/users", (req, res) => {});
+
+
+/**
+ * @name EmojiService
+ * @note These are the routes for anything related to a user.
+ * @note This is just general routing, You can modify as you want but before the delivery of the documentation
+ */
+app.get("/emoji", (req, res) => {});
+app.post("/emoji", (req, res) => {});
+app.put("/emoji", (req, res) => {});
+app.delete("/emoji", (req, res) => {});
+
+
+/**
+ * @name FlairService
+ * @note These are the routes for anything related to a user.
+ * @note This is just general routing, You can modify as you want but before the delivery of the documentation
+ */
+app.get("/flair", (req, res) => {});
+app.post("/flair", (req, res) => {});
+app.put("/flair", (req, res) => {});
+app.delete("/flair", (req, res) => {});
+
+
+/**
+ * @name CommentService
+ * @note These are the routes for anything related to a user.
+ * @note This is just general routing, You can modify as you want but before the delivery of the documentation
+ */
+app.get("/comment", (req, res) => {});
+app.post("/comment", (req, res) => {});
+app.put("/comment", (req, res) => {});
+app.delete("/comment", (req, res) => {});
+
+
+
+/**
+ * @name SrService
+ * @note These are the routes for anything related to a user.
+ * @note This is just general routing, You can modify as you want but before the delivery of the documentation
+ */
+app.get("/sr", (req, res) => {});
+app.post("/sr", (req, res) => {});
+app.put("/sr", (req, res) => {});
+app.delete("/sr", (req, res) => {});
+
+
+/**
+ * @name PMService
+ * @note These are the routes for anything related to a user.
+ * @note This is just general routing, You can modify as you want but before the delivery of the documentation
+ */
+app.get("/pm", (req, res) => {});
+app.post("/pm", (req, res) => {});
+app.put("/pm", (req, res) => {});
+app.delete("/pm", (req, res) => {});
+
+
+/**
+ * @name NotificationsService
+ * @note These are the routes for anything related to a user.
+ * @note This is just general routing, You can modify as you want but before the delivery of the documentation
+ * @description Should get notifications, mark them as read & deletes them.
+ * @description You can use EventEmitter in order to talk an action whenever something happens in the bakcend.
+ * @example
+ * let emitter = new EventEmitter();
+ * emitter.on("ev", (data) => {return data});
+ * emitter.emit("ev", data);
+ * @note don't think about SQL triggers, they're irrelevant in such case.
+ */
+app.get("/notif", (req, res) => {});
+app.post("/notif", (req, res) => {});
+app.put("/notif", (req, res) => {});
+app.delete("/notif", (req, res) => {});
+
+ app.listen(1337);
