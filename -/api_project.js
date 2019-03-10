98d3f88a08389b562c3ca18d0d499b73b3dcf441
Mostafa Hazem <mostafa.awad98@eng-st.cu.edu.eg>
2019-03-10 14:08:53 +0200

    Auto stash before merge of "master" and "origin/mostafahazem"

--- -/api_project.js
+++ -/api_project.js
@@ -7,7 +7,11 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
+<<<<<<< Updated upstream
     "time": "2019-03-10T11:33:14.241Z",
+=======
+    "time": "2019-03-10T11:55:45.051Z",
+>>>>>>> Stashed changes
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
