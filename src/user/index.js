fbb58ca7f26b50ac0e70a2647e1016b87abdc641
m1 <m1@hotmail.com>
2019-03-18 01:02:08 +0200

    complete user reg

--- src/user/index.js
+++ src/user/index.js
@@ -3,36 +3,49 @@ const express=require('express');
 const validator = require("email-validator");
 
 class UserHandler {
-    constructor(){
-
-    }
+    constructor(){}
 
     handleRegistration(req, res) {
-        
-         if (req.body.Password.length<8) {res.send("password too short");};
+        let Check=true;
+
+         if (req.body.Password.length<8) 
+         {
+             res.send("password too short");
+             Check=false;
+            };
 
          User.findOne({Username:req.body.Username}).then(function(user)
          {
                 if (user!==null)
                 {
                     res.send({error:'Username already exists'});
-                    res.end();
+                    Check=false
+                    
                 }
                 
          })
 
          User.findOne({Email:req.body.Email}).then(function(user)
          {
-                if (user!==null){res.send({error:'Email already exists'});}
+                if (user!==null)
+                {
+                    res.send({error:'Email already exists'});
+                    Check=false;
+                }
                 else 
                 {
                     if (validator.validate(req.body.Email)==false)
-                    {res.send({error:'Email invalid'});};
+                    {
+                        res.send({error:'Email invalid'});
+                        Check=false;
+                    };
                 }
                 
          })
 
-   User.create(req.body).then(function(user){res.send(user);})
+         if(Check)
+            { User.create(req.body).then(function(user){res.send(user);})}
+            else { res.send({error:'test'});}
     }
 }
 
