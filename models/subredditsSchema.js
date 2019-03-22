b19068f1e321ab485c0f0770c1ce4cabee3271fb
marwankefah <37806668+marwankefah@users.noreply.github.com>
2019-03-23 00:47:20 +0200

    merging subbreddits here for listing

---
+++ models/subredditsSchema.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create schema and model
+const SubredditPostSchema = new Schema({
+    title: String,
+    body: String
+});
+
+const SubredditSchema = new Schema({
+    name: String,
+    rules: [String],
+    posts: [SubredditPostSchema],
+    date: Date
+});
+
+
+
+const Subreddit = mongoose.model('subreddit', SubredditSchema);
+module.exports = Subreddit;
