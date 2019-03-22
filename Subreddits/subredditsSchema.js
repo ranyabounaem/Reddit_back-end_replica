9cf40be8559741b8596422ae08e7b90dd5fb8fb2
Rany Abounaem <ranyabounaem@gmail.com>
2019-03-22 18:53:54 +0200

    Updated schemas

--- Subreddits/subredditsSchema.js
+++ Subreddits/subredditsSchema.js
@@ -2,13 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 //Create schema and model
+const SubredditPostSchema = new Schema({
+    title: String,
+    body: [String]
+});
+
 const SubredditSchema = new Schema({
     name: String,
     rules: [String],
-    posts: [{title: String,
-            body: String }],
+    posts: [SubredditPostSchema],
     date: Date
 });
+
+
 
 const Subreddit = mongoose.model('subreddit', SubredditSchema);
 module.exports = Subreddit;
