5fd3c66199debf0e75ae79c9a203692a41554d84
Mostafa Hazem <mostafa.awad98@eng-st.cu.edu.eg>
2019-04-12 22:19:55 +0200

    Revert "integrated all tests together"
    This reverts commit 0f6d5f7f2deb73ea82441023022acd1e598c2ac3.

--- spec/user.spec.js
+++ spec/user.spec.js
@@ -7,7 +7,9 @@ describe("user tests", () => {
   beforeAll(() => {
     server = require("../index");
   });
-  
+  afterAll(() => {
+    server.close();
+  });
 
   describe("tests registering new users", () => {
     let data = {};
