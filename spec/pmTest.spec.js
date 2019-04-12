daa57871a60f6c8ba83384b78ca655a12b7385a1
Mostafa Hazem <mostafa.awad98@eng-st.cu.edu.eg>
2019-04-12 21:59:19 +0200

    Revert "Revert "Merge branch 'mostafa_hazem_usersphase1'""
    This reverts commit e4780bc6204213002df6dbe0671f6b41b11d4b49.

--- spec/pmTest.spec.js
+++ spec/pmTest.spec.js
@@ -24,11 +24,84 @@ describe('Server', function () {
         users.create({ Username: "saidahmed", Password: "12345678", Email: "mostafa@asdqqwm.com" });
         users.create({ Username: "soso", Password: "12345678", Email: "mostafa@msdsdaqqww.com" });
 
+const request = require('request');
+const mongoose = require('mongoose');
+const PMmongo = require('../models/PMmongo');
+const privateMessage = PMmongo.pm;
+const users = require('../models/UserSchema');
+const blockList = PMmongo.messageBlockList;
+const jasmine = require('jasmine');
+// testing compose/block/unblock/retrieveblock
+describe('Server', function () {
+    let server3;
+    beforeAll(function () {
+        server3 = require('../index.js');
+        // cause there is no middleware yet i need to add users to check for messages
+        users.create({ Username: "marwan", Password: "12345678", Email: "mostafa1@m.com" });
+        users.create({ Username: "kefah", Password: "12345678", Email: "mostafa2@m.com" });
+        users.create({ Username: "ahmed", Password: "12345678", Email: "mostafa3@m.com" });
+        users.create({ Username: "ahmedmohamed", Password: "12345678", Email: "mostafa4@m.com" });
+        users.create({ Username: "marawan", Password: "12345678", Email: "mostafa5@m.com" });
+        users.create({ Username: "hamada", Password: "12345678", Email: "mostafa6@m.com" });
+        users.create({ Username: "lolo", Password: "12345678", Email: "mostafa7@m.com" });
+        users.create({ Username: "koko", Password: "12345678", Email: "mostafa77@m.com" });
+        users.create({ Username: "mohamed", Password: "12345678", Email: "mostafa8@m.com" });
+        users.create({ Username: "saidahmed", Password: "12345678", Email: "mostafa9@m.com" });
+        users.create({ Username: "soso", Password: "12345678", Email: "mostafa99@m.com" });
 
     });
      afterAll(function () {
         server.close();
     });
+    describe('senderNotFoundError', function () {
+        let messageTest1 = {
+            sender: 'usernotindb', receiverUsername: "kefah", subject: "VIP", messageBody: "i love you"
+        };
+        let data = {};
+        beforeAll(function (done) {  // mocking the post request with message test
+            request.post('http://localhost:4000/' + messageTest1.sender + '/pm/compose',
+                { json: true, body: messageTest1 }, function (error, response, body) {
+                    data.status = response.statusCode;
+                    data.body = body;
+                    done();
+                });
+        });
+        //  status 404 for not found
+        it('Status 404', function () {
+            expect(data.status).toBe(404);
+            expect(data.body.error).toBe('userNotFound');
+        });
+        it('Database Test', function () {
+            privateMessage.findOne(messageTest1,
+                function (err, result) { expect(result).toBe(null); });
+        });
+    });
+    describe('receiverNotFoundError', function () {
+        let messageTest1 = {
+            sender: 'kefah', receiverUsername: "usernotindb", subject: "VIP", messageBody: "i love you"
+        };
+        let data = {};
+        beforeAll(function (done) {  // mocking the post request with message test
+            request.post('http://localhost:4000/' + messageTest1.sender + '/pm/compose',
+                { json: true, body: messageTest1 }, function (error, response, body) {
+                    data.status = response.statusCode;
+                    data.body = body;
+                    done();
+                });
+        });
+        //  status 404 for notfound
+        it('Status 404', function () {
+            expect(data.status).toBe(404);
+            expect(data.body.error).toBe('userNotFound');
+        });
+        it('Database Test', function () {
+            privateMessage.findOne(messageTest1,
+                function (err, result) { expect(result).toBe(null); });
+        });
+    });
+
+    });
+    
     describe('senderNotFoundError', function () {
         let messageTest1 = {
             sender: 'usernotindb', receiverUsername: "kefah", subject: "VIP", messageBody: "i love you"
@@ -290,6 +363,22 @@ describe('Server', function () {
                         });
                 });
 
+        });
+        //  status 403 for badrequest
+        it('Status 403', function () {
+            expect(data.status).toBe(403);
+            expect(data.body.error).toBe('blockedFromSending');
+        });
+        it('Database Test', function () {
+            privateMessage.findOne(messageTest1,
+                function (err, result) { expect(result).toBe(null); });
+        });
+        it('delete from  blocklist', function () {
+            blockList.findOneAndDelete({
+                blocker: 'ahmedmohamed',
+                blocked: 'saidahmed'
+            }, function (err, result) {
+                expect(result).not.toBe(null);
 
         });
         //  status 403 for badrequest
@@ -332,6 +421,16 @@ describe('Server', function () {
                         });
                 });
 
+        });
+        //  status 403 
+        it('Status 403', function () {
+            expect(data.status).toBe(403);
+            expect(data.body.error).toBe('blockedFromSending');
+        });
+        it('Database Test', function () {
+            privateMessage.findOne(messageTest1,
+                function (err, result) { expect(result).toBe(null); });
+        });
 
         });
         //  status 403 
@@ -507,6 +606,37 @@ describe('Server', function () {
             expect(data.body.error).toBe('selfBlockAlertError');
         });
 
+    });
+    describe('Blocking Someone ahmed block mohamed', function () {
+        // receiver username is undefined
+        let messageTest1 = {
+            blocked: 'mohamed',
+            blocker: 'ahmed'
+            , block: true
+        };
+        let messageTestFind = {
+            blocked: 'mohamed',
+            blocker: 'ahmed'
+        };
+        let data = {};
+        beforeAll(function (done) {  // mocking the post request with message test
+            request.post('http://localhost:4000/' + messageTest1.blocker + '/pm/block'
+                , { json: true, body: messageTest1 }, function (error, response, body) {
+                    data.status = response.statusCode;
+                    data.body = body;
+                    done();
+                });
+        });
+        //  status 200 for ok
+        it('Status 200', function () {
+            expect(data.status).toBe(200);
+            expect(data.body).toBe('OK');
+        });
+        it('Database Test', function () {
+            blockList.findOneAndDelete(messageTestFind,
+                function (err, result) {
+                    expect(result).not.toBe(null);
+                });
 
     });
     describe('Blocking Someone ahmed block mohamed', function () {
@@ -540,9 +670,9 @@ describe('Server', function () {
                     expect(result).not.toBe(null);
                 });
 
-
         });
 
+        });
 
 
     });
@@ -554,7 +684,16 @@ describe('Server', function () {
         };
         beforeAll(function (done) {
 
-            request.post('http://localhost:4000/' + owner + '/pm',
+    });
+    describe('Testing Message Retrieval Sent not inbox', function () {
+        let data = {};
+        let owner = 'marwan';
+        let testBody = {
+            mine: false
+        };
+        beforeAll(function (done) {
+
+            request.get('http://localhost:4000/' + owner + '/pm',
                 { json: true, body: testBody }, function (error, response, body) {
                     data.status = response.statusCode;
                     data.body = body;
@@ -587,7 +726,7 @@ describe('Server', function () {
         };
         beforeAll(function (done) {
 
-            request.post('http://localhost:4000/' + owner + '/pm',
+            request.get('http://localhost:4000/' + owner + '/pm',
                 { json: true, body: testBody }, function (error, response, body) {
                     data.status = response.statusCode;
                     data.body = body;
@@ -615,6 +754,23 @@ describe('Server', function () {
 
     });
 
+    describe('unblocking Someone ahmed unblock mohamed after blocking him', function () {
+        // receiver username is undefined
+        let messageTest1 = {
+            blocked: 'mohamed',
+            blocker: 'ahmed'
+            , block: true
+        };
+        let messageTestFind = {
+            blocked: 'mohamed',
+            blocker: 'ahmed'
+        };
+        let data = {};
+        beforeAll(function (done) {  // mocking the post request with message test
+            request.post('http://localhost:4000/' + messageTest1.blocker + '/pm/block'
+                , { json: true, body: messageTest1 }, function (error, response, body) {
+                    data.status = response.statusCode;
+                    data.body = body;
 
     describe('unblocking Someone ahmed unblock mohamed after blocking him', function () {
         // receiver username is undefined
@@ -665,13 +821,14 @@ describe('Server', function () {
                     expect(result).toBe(null);
                 });
 
+        });
 
         });
 
 
-
     });
 
+    });
 
     describe('unblocking Someone not in  block list', function () {
         // receiver username is undefined
@@ -704,6 +861,66 @@ describe('Server', function () {
                     expect(result).toBe(null);
                 });
 
+    describe('unblocking Someone not in  block list', function () {
+        // receiver username is undefined
+        let messageTest1 = {
+            blocked: 'lolo',
+            blocker: 'koko'
+            , block: false
+        };
+        let messageTestFind = {
+            blocked: 'lolo',
+            blocker: 'koko'
+        };
+        let data = {};
+        beforeAll(function (done) {  // mocking the post request with message test
+            request.post('http://localhost:4000/' + messageTest1.blocker + '/pm/block'
+                , { json: true, body: messageTest1 }, function (error, response, body) {
+                    data.status = response.statusCode;
+                    data.body = body;
+                    done();
+                });
+        });
+        //  status 200 for ok
+        it('Status 403', function () {
+            expect(data.status).toBe(403);
+            expect(data.body.error).toBe('canNotBUnblockNonBlockedUser');
+        });
+        it('Database Test', function () {
+            blockList.findOne(messageTestFind,
+                function (err, result) {
+                    expect(result).toBe(null);
+                });
+
+        });
+    });
+    describe('retriving  block list', function () {
+        // receiver username is undefined
+        let messageTest1 = {
+            blocked: 'soso',
+            blocker: 'lolo'
+            , block: true
+        };
+        let messageTest2 = {
+            blocked: 'koko',
+            blocker: 'lolo'
+            , block: true
+        };
+        let messageTestFind = {
+            blocker: 'lolo',
+            blocked: 'koko'
+        };
+        let messageTestFind2 = {
+            blocker: 'lolo',
+            blocked: 'soso'
+        };
+        let data = {};
+        // makking a user block two others and then retriving them
+        beforeAll(function (done) {  // mocking the post request with message test
+            request.post('http://localhost:4000/' + messageTest1.blocker + '/pm/block'
+                , { json: true, body: messageTest1 }, function (err) {
+                    request.post('http://localhost:4000/' + messageTest2.blocker + '/pm/block'
+                        , { json: true, body: messageTest2 }, function (err) {
 
         });
     });
@@ -738,12 +955,20 @@ describe('Server', function () {
                             done();
                         });
 
+                });
 
                 });
 
+        });
 
         });
 
+        beforeEach(function (done) {
+            request.get('http://localhost:4000/' + messageTest1.blocker + '/pm/blocklist'
+                , function (error, response, body) {
+                    data.status = response.statusCode;
+                    data.body = body;
+                    done();
 
         beforeEach(function (done) {
             request.get('http://localhost:4000/' + messageTest1.blocker + '/pm/blocklist'
@@ -755,6 +980,16 @@ describe('Server', function () {
                 });
 
 
+        });
+        //  status 200 for ok
+        it('Status 200', function () {
+            expect(data.status).toBe(200);
+        });
+        it('Database Test', function () {
+            blockList.findOneAndDelete(messageTestFind,
+                function (err, result) {
+                    expect(result).not.toBe(null);
+                });
 
         });
         //  status 200 for ok
@@ -775,12 +1010,13 @@ describe('Server', function () {
         });
 
 
+    });
 
     });
 
 
 
+});
 
 });
 
- */
