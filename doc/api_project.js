c33631606148333a7619783394f4603f4bacb3a5
Rany Abounaem <ranyabounaem@gmail.com>
2019-04-13 14:14:29 +0200

    Edited doc folder

--- doc/api_project.js
+++ doc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-04-13T11:56:24.157Z",
+    "time": "2019-04-13T12:13:33.399Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
